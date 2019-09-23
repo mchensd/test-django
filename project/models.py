c604b08301d9d87344c047c84716a7b609d25f3f
Michael  Chen <chen.young@Michaels-MacBook-Pro.local>
2019-09-22 21:04:45 -0400

    added author field

--- project/models.py
+++ project/models.py
@@ -1,9 +1,12 @@
 from django.db import models
-
-
+from django.conf import settings
+from django.contrib.auth.models import User
 # Create your models here.
 class Counter(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=User.objects.all()[0].pk)
     count = models.IntegerField()
     name = models.CharField(max_length=128, default="")
     def __str__(self):
-        return self.name + " " + str(self.count)
+         self.name + " " + str(self.count)
+
+
